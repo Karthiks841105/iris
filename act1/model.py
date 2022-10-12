4e4b9df56412645ba0d8eb35424e7c6f2054ec41
Karthiks841105 <99671277+Karthiks841105@users.noreply.github.com>
2022-10-12 14:48:04 +0530

    Update model.py

--- act1/model.py
+++ act1/model.py
@@ -1,42 +1,28 @@
-import streamlit as st 
-from PIL import Image
-import numpy as np
-from flask import Flask, request, jsonify, render_template
 import pickle
-import json
-import pandas as pd
-app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+from sklearn import datasets
+iris=datasets.load_iris()
+x=iris.data
+y=iris.target
+#labels for iris dataset
 labels ={
  0: "setosa",
  1: "versicolor",
  2: "virginica"
 }
-@app.route('/')
-def welcome():
-	return "Index Page"
-@app.route('/predict',methods=['POST'])
-def predict(sl,sw,pl,pw):
-    prediction=model.predict([[sl,sw,pl,pw]])
-    return labels[prediction[0]]
-def main():
-    st.title("IRIS Prediction")
-    html_temp = """
- 	<div style="background-color:tomato;padding:10px">
- 	<h2 style="color:white;text-align:center;">Streamlit IRIS Predictor </h2>
- 	</div>
- 	"""
-    st.markdown(html_temp,unsafe_allow_html=True)
-    sl = st.text_input("Sepal Length","Type Here")
-    sw = st.text_input("Sepal Width","Type Here")
-    pl = st.text_input("Petal Length","Type Here")
-    pw = st.text_input("Petal Width","Type Here")
-    result=""
-    if st.button("Predict"):
-        result=predict(sl,sw,pl,pw)
-    st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
-if __name__=='__main__':
- main()
+#split the data set
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.50)
+#Using decision tree algorithm
+from sklearn import tree
+classifier=tree.DecisionTreeClassifier()
+classifier.fit(x_train,y_train)
+predictions=classifier.predict(x_test)
+#export the model
+pickle.dump(classifier, open('model.pkl','wb'))
+#load the model and test with a custom input
+model = pickle.load( open('model.pkl','rb'))
+x = [[6.7, 3.3, 5.7, 2.1]]
+predict = model.predict(x)
+print("Hello Worlds")
+print(labels[predict[0]])
+
